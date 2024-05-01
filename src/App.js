667e3c14574d6a7413af2fc27cda4dac3ea43692
Beingswhowereborn <dolmin0708@naver.com>
2024-05-01 12:06:49 +0900

    article map 함수까지 진행

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import Button from "react-bootstrap; // 꼭 import를 해와야한다
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import bg from "./img/bg.png";
+import data from "./data.js";
+import { useState } from "react";
 
 function App() {
+  let [article, setArticle] = useState(data);
+
   return (
     <div className="App">
-      <div>어렵다 어려워</div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+
+      <div className="container">
+        <div className="row">
+          {article.map((a, i) => {
+            return <Card article={article[i]} i={i + 1}></Card>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.article.title}</h4>
+      <p>{props.article.price}</p>
     </div>
   );
 }
