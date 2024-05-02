c7f294678277484a2f7778b2ae28007319b4acb7
beingborn <dolmin0708@naver.com>
2024-05-02 09:40:59 +0900

    Detail page까지 진행

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import bg from "./img/bg.png";
 import data from "./data.js";
 import { useState } from "react";
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   let [article, setArticle] = useState(data);
 
   return (
     <div className="App">
+
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -22,19 +24,35 @@ function App() {
         </Container>
       </Navbar>
 
-      <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + bg + ")" }}
-      ></div>
-
-      <div className="container">
-        <div className="row">
-          {article.map((a, i) => {
-            return <Card article={article[i]} i={i + 1}></Card>;
+    <Routes>
+      <Route path="/" element={ <>
+        <div className="main-bg"></div>
+        <div className="container">
+          <div className="row">
+          { article.map((a, i)=>{
+            return <Card article={article[i]} i={i} ></Card>
           })}
         </div>
+        </div>
+        
+      </> } />
+      <Route path="/detail" element={ <div>상세페이지임</div> } />
+      <Route path="/about" element={ <div>어바웃페이지임</div> } />
+    </Routes>
+
+ 
+
+
+
+
+      <div className="footer">
+        <Link to="/">홈</Link>
+        <Link to="/detail">상세페이지</Link>
+        <Link to="/event">이벤트페이지</Link>
       </div>
+{/* App */}
     </div>
+    
   );
 }
 
@@ -42,7 +60,7 @@ function Card(props) {
   return (
     <div className="col-md-4">
       <img
-        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        src={"https://codingapple1.github.io/shop/shoes" + (props.i + 1 )+ ".jpg"}
         width="80%"
       />
       <h4>{props.article.title}</h4>
