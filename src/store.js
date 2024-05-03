8c687b2636e447ec2b46dcae4e694dd21d66e57f
beingborn <dolmin0708@naver.com>
2024-05-03 11:52:58 +0900

    버튼 누를 시 해당 상품 숫자 값 증가시키기 해야함

--- src/store.js
+++ src/store.js
@@ -15,11 +15,14 @@ let cart = createSlice({
   name: 'cart',
   initialState: [
     { id: 0, name: 'White and Black', count: 2 },
-    { id: 2, name: 'Grey Yordan', count: 1 },
-    { id: 3, name: '오징어 젓갈', count: 5 },
-    { id: 4, name: '메인리두서', count: 1 },
-    { id: 5, name: '메롱메롤', count: 1 },
+    { id: 1, name: 'Grey Yordan', count: 1 },
+    { id: 2, name: '오징어 젓갈', count: 5 },
+    { id: 3, name: '메인리두서', count: 1 },
+    { id: 4, name: '메롱메롤', count: 1 },
   ],
+
+
+  // 첫번째로 우선 모든 카운트를 늘리는 것부터 구현하는 게 좋을 것 같다.
 
 });
 
