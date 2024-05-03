b1444970b2cb5f719056cbdab069e36ca44798d7
beingborn <dolmin0708@naver.com>
2024-05-03 11:10:19 +0900

    장바구니 데이터 바인딩 진행

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,33 +1,66 @@
 import {Table} from "react-bootstrap" 
 import { useState } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { changeName , changeAge } from "./../store.js"
 
 function Cart(){
   let [bookmark, setBook] = useState(["오징어", "낙지"])
+  
 
-  let a = useSelector((state)=>{return state})
-  console.log(a.stock[0])
+  // let a = useSelector((state)=>{return state})
+  // console.log(a.stock[0])
+
+  // useSelector 편하게 쓰는 법
+
+  // let a = useSelector((state)=>state.user)
+
+
+  let username = useSelector((state) => state.user.name )
+  let userage = useSelector((state) => state.user.age)
+  // let [cartItem, setCart] = useState([useSelector((state) => state.cart)])
+  //   console.log(cartItem)
+
+
+  let cartItem = useSelector((state)=> state.cart)
+  let dispatch = useDispatch()                      // store.js 로 요청 보내주는 함수
 
   return(
 
 
 <div>
+
+  {username}의 장바구니
+  {userage}살
+  <button onClick={()=>{dispatch(changeAge())}}>나이를 먹자</button>
 <Table>
   <thead>
     <tr>
-      <th>{bookmark[0]}</th>
-      <th>{a.user}</th>
-      <th>{a.stock[0]}</th>
+      <th>상품인덱스</th>
+      <th>상품명</th>
+      <th>재고개수</th>
       <th>변경하기</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>안녕</td>
-      <td>안녕</td>
-      <td>안녕</td>
-    </tr>
+      {/* <td>{cartItem[0].id}</td>
+      <td>{cartItem[0].name}</td>
+      <td>{cartItem[0].count}</td> */}
+
+
+      {/* 반복문 사용시 key 속성을 사용해 주면 좋다. */}
+      {cartItem.map((a,i)=>{
+          return <tr key={i}>
+                <td>{cartItem[i].id}번</td>
+                <td>{cartItem[i].name}</td>
+                <td>{cartItem[i].count}개</td>
+                <td><button onClick={()=>{ dispatch(changeName())}}>+</button></td> {/* dispatch 는 store.js 에게 메시지를 보내는 거라 생각 */}
+            </tr>
+           
+          
+        })
+      }
+
+   
   </tbody>
 </Table> 
 </div>    
--- src/store.js
+++ src/store.js
@@ -3,10 +3,39 @@ import { createStore } from 'redux';
 
 // const store = createStore(rootReducer);
 
+//  Redux의 state 변경 법 
+// - state 수정 함수 만들기
+
 let user = createSlice({
   name : 'user',                                // state 이름
-  initialState : 'KIM'                     // state 값  
+  initialState : { name : 'kim' , age : 20},// state 값
+  reducers : {
+    changeName(state) { // state 파라미터 추가 시 기존 스테이트를 뜻함
+      state.name = 'minhuk kim'
+    },
+
+    changeAge(state){
+      state.age += 1
+    },
+  }  
 })
+
+  export let { changeName, changeAge } = user.actions
+
+
+let cart = createSlice({
+  name: 'cart',
+  initialState: [
+    { id: 0, name: 'White and Black', count: 2 },
+    { id: 2, name: 'Grey Yordan', count: 1 },
+    { id: 3, name: '오징어 젓갈', count: 5 },
+    { id: 4, name: '메인리두서', count: 1 },
+    { id: 5, name: '메롱메롤', count: 1 },
+  ],
+
+});
+
+
 
 let stock = createSlice({
   name : 'stock',                                // state 이름
@@ -18,6 +47,7 @@ let stock = createSlice({
 export default configureStore({
   reducer: { 
     user : user.reducer,
-    stock : stock.reducer
+    stock : stock.reducer,
+    cart : cart.reducer,
   }
 }) 
